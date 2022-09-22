16c52c521a49ac97cbdb3faac894138633fb4917
t-flim <fulim1988@gmail.com>
2022-09-22 17:16:52 +0800

    Add weather api handling logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -10,5 +11,34 @@ app.use(
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/weather", (req, res) => {
+  const endPoint = process.env.WEATHER_API_URL;
+  const appId = process.env.WEATHER_API_ID;
+  const { cityName, units } = req.query;
+  const unitMeasurement = units ? units : "metric";
+  const params = `q=${cityName}&units=${unitMeasurement}&appId=${appId}`;
+  const weatherUrl = `${endPoint}weather?${params}`;
+  const forecastUrl = `${endPoint}forecast?${params}`;
+
+  // res.send({weatherUrl, forecastUrl});
+  const weatherReq = fetch(weatherUrl);
+  const forecastReq = fetch(forecastUrl);
+
+  Promise.all([weatherReq, forecastReq])
+    .then((responses) => {
+      return Promise.all(
+        responses.map((response) => {
+          return response.json();
+        })
+      );
+    })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = app;
